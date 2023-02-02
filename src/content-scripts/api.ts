986f1a557d573e0876d9fe98c407f60dda9ba6fc
nhaouari <nhaouari@users.noreply.github.com>
2023-02-02 21:55:58 +0100

    use directly duckduckgo

--- src/content-scripts/api.ts
+++ src/content-scripts/api.ts
@@ -5,19 +5,15 @@ export interface SearchResult {
 }
 
 export async function apiSearch(query: string, numResults: number, timePeriod: string, region: string): Promise<SearchResult[]> {
-    const url = `https://ddg-webapp-aagd.vercel.app/search?`
-        + `max_results=${numResults}`
-        + `&q=${query}`
-        + (timePeriod ? `&time=${timePeriod}` : "")
-        + (region ? `&region=${region}` : "")
+    const url = `http://api.duckduckgo.com/?q=${query}&format=json`
 
     const response = await fetch(url)
     const results = await response.json()
-    return results.map((result: any) => {
+    return results.RelatedTopics.map((result: any) => {
         return {
-            body: result.body,
-            href: result.href,
-            title: result.title
+            body: result.Text,
+            href: result.FirstURL,
+            title: result.Text
         }
     })
 }
